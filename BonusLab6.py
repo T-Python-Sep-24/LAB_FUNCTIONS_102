8ef69d7f6b38532a8d78e8dadf58c746fec36217
KhuloodGhazi <khulood.gmo@gmail.com>
2024-10-05 20:28:09 +0300

    I DID IT :

--- BonusLab6.py
+++ BonusLab6.py
@@ -1,2 +1,18 @@
 #Bonus Lab 6
-#sorry can't sovle it
+
+def myCapital(inputString):
+    if not isinstance(inputString, str):
+        return "Input is not a string"
+    
+    result = ''
+    
+    for char in inputString:
+        if char.isupper():
+            result += ' ' + char.lower()
+        else:
+            result += char
+    
+    return result.strip()
+
+
+print(myCapital("helloWorldThere"))
